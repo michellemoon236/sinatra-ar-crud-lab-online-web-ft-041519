e8f70164043b45ed51ec37c891b49b18baf1cfe8
Michelle Moon <michellemoon236@gmail.com>
2019-06-06 20:30:39 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,8 @@ class ApplicationController < Sinatra::Base
   end
   
   post '/articles' do
-    article = Article.create(title: params[:title], content: params[:content])
-    redirect "/articles/#{article.id}"
+    @article = Article.create(title: params[:title], content: params[:content])
+    redirect "/articles/#{@article.id}"
   end
   
   get '/articles' do
@@ -36,8 +36,8 @@ class ApplicationController < Sinatra::Base
   end
   
   patch '/articles/:id' do
-    article = Article.find(params[:id])
-    article.update(title: params[:title], content: params[:content])
+    @article = Article.find(params[:id])
+    @article.update(title: params[:title], content: params[:content])
     redirect "/articles/#{article.id}"
   end
   
